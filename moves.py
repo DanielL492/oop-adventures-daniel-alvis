b03e3ef8c0708fe1b41fcd6c1866e0ae9afa0f0c
DannyL1234 <124058520+DannyL1234@users.noreply.github.com>
2023-05-04 08:29:32 -0400

    HI

--- moves.py
+++ moves.py
@@ -1,7 +1,33 @@
+class mob:
+    def __init__(self, name):
+        self.name = name
+class zombie(mob):
+    def __init__(self, name, hp, atk, defe, heal):
+        super().__init__(name)
+        self.hp = hp
+        self.atk = atk 
+        self.defe = defe
+        self.heal = heal
+class enderman(mob):
+     def __init__(self, name, ehp, eatk, edefe, eheal):
+        super().__init__(name)
+        self.ehp = ehp
+        self.eatk = eatk
+        self.edefe = edefe
+        self.eheal = eheal
+class ravager(mob):
+    def __init__(self, name, rhp, ratk, rdefe, rheal):
+        super().__init__(name)
+        self.rhp = rhp
+        self.ratk = ratk 
+        self.defe = rdefe
+        self.rheal = rheal
+
 import random
 guess = input("guess")
 x = random.randint(1, 3)
-if int(guess) == 
+if int(guess) == 1:
+    
 
 hp = 20
 atk = 4
