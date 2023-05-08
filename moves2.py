2e73e04449d8297be3cdb3e4fffe94812609ac0c
Seebso <alru082019@gmail.com>
2023-05-08 08:37:29 -0400

    asdwasdw

--- moves2.py
+++ moves2.py
@@ -2,30 +2,32 @@ class mob:
     def __init__(self, name):
         self.name = name
 class zombie(mob):
-    def __init__(self, name, hp, atk, defe, heal):
+    def __init__(self, name, zhp, zatk, zdefe):
         super().__init__(name)
-        self.hp = hp
-        self.atk = atk 
-        self.defe = defe
-        self.heal = heal
+        self.zhp = zhp
+        self.zatk = zatk 
+        self.zdefe = zdefe
+    def __str__(self):
+        return f"{self.zhp}, {self.zatk}, {self.zdefe}"
 class enderman(mob):
-     def __init__(self, name, ehp, eatk, edefe, eheal):
+    def __init__(self, name, ehp, eatk, edefe, eheal):
         super().__init__(name)
         self.ehp = ehp
         self.eatk = eatk
         self.edefe = edefe
         self.eheal = eheal
+    def __str__(self):
+        return f"{self.ehp}, {self.eatk}, {self.edefe}, {self.eheal}"
 class ravager(mob):
     def __init__(self, name, rhp, ratk, rdefe, rheal):
         super().__init__(name)
         self.rhp = rhp
         self.ratk = ratk 
-        self.defe = rdefe
+        self.rdefe = rdefe
         self.rheal = rheal
-
-import random
-guess = input("guess")
-x = random.randint(1, 3) 
+    def __str__(self):
+        return f"{self.rhp}, {self.ratk}, {self.rdefe}, {self.rheal}"
+    
 hp = 20
 atk = 4
 defe = 6
@@ -58,4 +60,5 @@ if move == "attack":
 elif move == "heal":
     hp = hp + heal
     print(hp)
+
     
